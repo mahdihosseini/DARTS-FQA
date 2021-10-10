0d7113bc61df08f97bae7304fee2462e7dde4423
adynmiles <adynmiles.am@gmail.com>
2021-10-10 16:26:56 -0400

    heatmap_demo img_path changed

--- heatmap_demo.sh
+++ heatmap_demo.sh
@@ -4,7 +4,7 @@ python heatmap_creation.py \
 --arch DARTS-FQA \
 --layers 3 \
 --init_channels 20 \
---img 'D:/WSI Images for HeatMap/WSI_1/TCGA-02-0111-01A-01-TS1.89374f94-22f0-4c3d-a890-ee6082c14bd5.svs' \
+--img ./whole_slide_images/example_1.svs \
 --genotype focuspath \
 --trainset focuspath64 \
 --checkpoint_path ./pretrained_models/darts-fqa_focuspath.pt \
